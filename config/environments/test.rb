54b952e7f4788142ef7048ff282743efea6747c9
zeus <ishan.h@outlook.com>
2014-10-16 23:50:44 -0700

    Finish user Sign-up

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,4 +33,7 @@ SampleApp::Application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
+  #chapter 7 v2 rails 4 bcrypt bullcrap
+    ActiveModel::SecurePassword.min_cost = true
+
 end
