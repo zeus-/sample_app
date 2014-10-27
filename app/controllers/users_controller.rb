4cd0ca1596e5923ec6db6761dfaf1aa9f8aa92fe
zeus <ishan.h@outlook.com>
2014-10-27 00:16:21 -0700

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -38,7 +38,7 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    User.find(params[:id].destroy)
+    User.find(params[:id]).destroy
     flash[:success] = "User deleted."
     redirect_to users_url
   end
